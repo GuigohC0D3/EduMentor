97c3b323434fc6f885cc1cf0ccca204838d25180
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2024-06-16 15:15:59 -0300

    Finalizei o Forget e comecei a produzir a HomePage

--- edu-mentor/src/App.jsx
+++ edu-mentor/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import LoginForm from "./assets/components/Logins/LoginForm";
 import ForgetPassword from "./assets/components/Forget/ForgetPassword";
 import RegisterForm from "./assets/components/Register/RegisterForm";
+import HomePage from "./assets/components/Home/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/Login" element={<LoginForm />} />
         <Route path="/Register" element={<RegisterForm />} />
         <Route path="/Forget" element={<ForgetPassword />} />
+        <Route path="/Home" element={<HomePage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
---
+++ edu-mentor/src/assets/components/Home/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HomePage.css"; // Importando estilos
+
+const HomePage = () => {
+  return <main id="container"></main>;
+};
+
+export default HomePage;
